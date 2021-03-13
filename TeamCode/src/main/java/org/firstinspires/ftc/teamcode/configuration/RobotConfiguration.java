fe25d948a6d115fbb001a84cb81891fe2d5f6233
Tazering <tyler.ys.kim@gmail.com>
2021-03-12 21:01:42 -0500

    Initial Gyro Sensor Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configuration/RobotConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configuration/RobotConfiguration.java
@@ -1,25 +1,22 @@
 package org.firstinspires.ftc.teamcode.configuration;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-public class RobotConfiguration
-{
+public class RobotConfiguration {
     /* Public OpMode members. */
-    public DcMotor leftDrive = null;
-    public DcMotor rightDrive = null;
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+    public BNO055IMU imu;
 
 
     /* local OpMode members. */
-    HardwareMap hwMap =  null;
+    HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
-
-    /* Constructor */
-    public RobotConfiguration() {
-
-    }
 
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
@@ -27,7 +24,7 @@ public class RobotConfiguration
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        leftDrive = hwMap.get(DcMotor.class, "left_drive");
         rightDrive = hwMap.get(DcMotor.class, "right_drive");
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
@@ -36,13 +33,28 @@ public class RobotConfiguration
         leftDrive.setPower(0);
         rightDrive.setPower(0);
 
-
-
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
- }
+
+    public void initGyroSensor(HardwareMap hardwareMap) {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+    }
+}
 

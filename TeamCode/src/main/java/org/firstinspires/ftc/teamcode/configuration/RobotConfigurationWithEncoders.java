2be2fe7d5f4601d68c72a935c1cfab6ccb818fcc
Tazering <tyler.ys.kim@gmail.com>
2021-04-09 20:33:50 -0400

    Added Encoder Class Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configuration/RobotConfigurationWithEncoders.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.configuration;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+public class RobotConfigurationWithEncoders {
+    /* Public OpMode members. */
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+    public BNO055IMU imu;
+    public ColorSensor colorSensor;
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftDrive = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+        colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motor
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void initGyroSensor(HardwareMap hardwareMap) {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/RevColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/RevColorSensorTest.java
@@ -1,19 +1,13 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
-        import android.app.Activity;
         import android.graphics.Color;
-        import android.view.View;
 
-        import com.qualcomm.robotcore.eventloop.opmode.Disabled;
         import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
         import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
         import com.qualcomm.robotcore.hardware.ColorSensor;
-        import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-        import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
         import org.firstinspires.ftc.teamcode.configuration.RobotConfiguration;
 
-        import java.util.Locale;
 
 
 @TeleOp(name = "Sensor: REVColorDistance", group = "Sensor")
@@ -37,10 +31,10 @@ public class RevColorSensorTest extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            Color.RGBToHSV((int) (sensorColor.red()),
-                    (int) (sensorColor.green()),
-                    (int) (sensorColor.blue()),
-                    hsvValues);
+//            Color.RGBToHSV((int) (sensorColor.red()),
+//                    (int) (sensorColor.green()),
+//                    (int) (sensorColor.blue()),
+//                    hsvValues);
 
             telemetry.addData("Red  ", sensorColor.red());
             telemetry.addData("Green", sensorColor.green());
